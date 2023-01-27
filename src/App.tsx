611875480accba3220028935e57fc128db027314
IbrahimYemi <ibrahimsharafadeen95@gmail.com>
2023-01-27 14:28:32 +0100

    Project commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from './app/dashboard/Home';
+import Login from './app/login/login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import User from './app/dashboard/User';
 
-function App() {
+const App: React.FC = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/users" caseSensitive={false} element={<Home />} />
+          <Route path="/user/details" caseSensitive={false} element={<User />} />
+          <Route path="/" caseSensitive={false} element={<Login />} />
+          <Route path="*" caseSensitive={false} element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
